cce767bec011d1dce687870bf47225e01bca99a1
Douglas Sales <doug.sales23@gmail.com>
2020-04-21 23:29:43 -0300

    Add features required by tests.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    api.post('repositories', {
+      title: 'Desafio ReactJS',
+      url: 'http://localhost:3333/repository',
+      techs: []
+    }).then(response => {
+      setRepositories([...repositories, response.data])
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(() => {
+      const repositoriesNew = repositories.filter(repository => repository.id !== id);
+
+      setRepositories(repositoriesNew);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        { repositories.map( repository => (
+        <li key = { repository.id }>
+          {repository.title}
+      
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
